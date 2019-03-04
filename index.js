5a0316e0ab566069da3ec9f6400538b92772d650
Antariksh C <antarikshc@gmail.com>
2019-03-05 02:28:12 +0530

    feat: Create DB conn and inject in User DAO

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+import mongoose from 'mongoose';
 import app from './server';
+import UsersDAO from './src/models/users.dao';
+
+require('dotenv').config();
 
 // Set PORT
 app.set('port', 4040);
 
-app.listen(app.get('port'), () => {
-  console.log(`Server is running on ${app.get('port')}`);
-});
+// Create connection to Database
+mongoose.createConnection(String(process.env.DB_URI), { useNewUrlParser: true })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    // We got the connection to DB, inject to DAO
+    await UsersDAO.injectDB(client);
+
+    // Proceed to listen for incoming requests
+    app.listen(app.get('port'), () => {
+      console.log(`Server is running on ${app.get('port')}`);
+    });
+  });
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import users from './src/api/user.route';
 
 const app = express();
 
@@ -7,7 +8,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Register API routes
+app.use('/users', users);
 app.get('/', (req, res) => { res.send('Welcome to SkillStead API'); });
-app.use('*', (req, res) => res.status(404).json({ error: 'You seem lost dear...' }));
+app.use('*', (req, res) => res.status(404).json({ error: 'You seem lost a bit lost' }));
 
 export default app;
