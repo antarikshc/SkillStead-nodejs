1f9b856d163e3e72912b3703905201cee20d18ce
Antariksh C <antarikshc@gmail.com>
2019-03-05 02:26:32 +0530

    feat: Add User schema

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,29 +1,25 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 // Setup userSchema
-let userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: "Name cannot be blank!"
-    },
-    email: {
-        type: String,
-        required: "Email-ID cannot be blank!"
-    },
-    mobile: String,
-    headline: String,
-    avatar: {
-        type: Number,
-        default: 14
-    },
-    password: {
-        type: String,
-        select: false   // Won't include in response
-    },
-    fcm: [{
-        token: String
-    }]
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'Name cannot be blank!'
+  },
+  email: {
+    type: String,
+    required: 'Email-ID cannot be blank!'
+  },
+  mobile: String,
+  headline: String,
+  password: {
+    type: String,
+    select: false // Won't include in response
+  },
+  fcm: [{
+    token: String
+  }]
 });
 
 // Create and Export schema
-module.exports = mongoose.model("User", userSchema);
+module.exports = userSchema;
