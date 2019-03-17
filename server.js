d78857544e4934bfce269e3e2ebcbb6549023426
Antariksh C <antarikshc@gmail.com>
2019-03-17 21:45:32 +0530

    feat: Enable CORS for local testing

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import users from './src/api/user.route';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // Register API routes
 app.use('/users', users);
