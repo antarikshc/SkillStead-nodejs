227fd133f73cb0d86a6c6d5e992db345a6dc3b12
Antariksh C <antarikshc@gmail.com>
2019-03-22 22:43:00 +0530

    feat: Route /category

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import users from './src/api/user.route';
+import category from './src/api/category.route';
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 
 // Register API routes
 app.use('/users', users);
+app.use('/category', category);
 app.get('/', (req, res) => { res.send('Welcome to SkillStead API'); });
 app.use('*', (req, res) => res.status(404).json({ error: 'You seem lost a bit lost' }));
 
