78f6f32e383f689bcd26df64ac4824b673b5319d
Antariksh C <antarikshc@gmail.com>
2019-03-24 14:59:43 +0530

    feat: add listeners to handle queue

--- src/game/main.socket.js
+++ src/game/main.socket.js
@@ -1,6 +1,8 @@
 import socket from 'socket.io';
+import _ from 'lodash';
 
 let io;
+const queue = [];
 
 export default class SocketController {
   /**
@@ -13,7 +15,23 @@ export default class SocketController {
     io.on('connection', (client) => {
       console.log(`Client ${client.id} has been connected`);
 
+      client.on('userJoinQueue', () => {
+        queue.push({ socketId: client.id });
+        console.log(`${client.id} added to match-queue`);
+      });
+
+      client.on('userLeaveQueue', () => {
+        if (queue.filter(item => item.socketId === client.id).length > 0) {
+          _.remove(queue, item => item.socketId === client.id);
+          console.log(`${client.id} removed from match-queue`);
+        }
+      });
+
       client.on('disconnect', () => {
+        if (queue.filter(item => item.socketId === client.id).length > 0) {
+          _.remove(queue, item => item.socketId === client.id);
+          console.log(`${client.id} removed from match-queue`);
+        }
         console.log(`Client ${client.id} has been disconnected`);
       });
     });
